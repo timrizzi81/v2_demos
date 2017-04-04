0c2bb092314e950d23095c56c404e7ae5679a1ee
Colin Schmidt <colinjschmidt@gmail.com>
2017-04-04 14:31:28 -0700

    update

--- createFile.py
+++ createFile.py
@@ -4,6 +4,8 @@ file = open("report.md","w")
 file.write("# Sample Report")
 file.close()
 
+print("test")
+
 os.system("git add .")
 os.system("git commit -m 'testing automated git commit'")
 os.system("git pull origin master")
