826f98a13afe39511d552f66f2de39adfe41c6c3
timrizzi81 <tim@datascience.com>
2017-04-04 22:02:36 +0000

    updating create report script

--- createFile.py
+++ createFile.py
@@ -1,7 +1,8 @@
 import os
 
 file = open("report.md","w")
-file.write("# Sample Report 3")
+file.write("# Sample Report 6")
+file.write("![platform image](https://raw.githubusercontent.com/timrizzi81/v2_demos/master/Platform%20Visual.png)")
 file.close()
 
 os.system('git config --global user.email "' + os.environ['GIT_EMAIL'] + '"')
