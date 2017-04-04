a3369fb4282d592516f08ecdbcf179841aa52bd4
timrizzi81 <tim@datascience.com>
2017-04-04 21:04:54 +0000

    Updating createFile.py

--- createFile.py
+++ createFile.py
@@ -1,10 +1,10 @@
 import os
 
 file = open("report.md","w")
-file.write("# Sample Report")
+file.write("# Sample Report 2")
 file.close()
 
 os.system("git add .")
-os.system("git commit -m 'testing automated git commit'")
+os.system("git commit -m 'Automated git commit from run'")
 os.system("git pull origin master")
 os.system("git push origin master")
